5e47e93ddc539fa8a11e1569b0b08362ab273c0d
Lars <larsjohnarciaga.tagle@bicol-u.edu.ph>
2025-07-11 15:23:47 +0800

    Create new screens and components

---
+++ screens/InvoiceFormScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function InvoiceFormScreen() {
+  return (
+    <View>
+      <Text>InvoiceFormScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ screens/InvoiceListScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function InvoiceListScreen() {
+  return (
+    <View>
+      <Text>InvoiceListScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
